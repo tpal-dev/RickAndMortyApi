d965595c84fb54eaa529d96ed44e8e8ab88bd0f2
tpal-dev <tpal.developer@gmail.com>
2024-02-17 16:43:26 +0100

    Initial Commit

---
+++ RickAndMortyApi/RickAndMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApiApp.swift
+//  RickAndMortyApi
+//
+//  Created by Tomasz Paluszkiewicz on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
